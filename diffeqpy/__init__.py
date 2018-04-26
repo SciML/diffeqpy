e92b71fe2902400ae33d98a3de06677a480fe430
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-25 23:25:22 -0700

    Bundle DiffEqPy.jl into diffeqpy
    closes #22

--- diffeqpy/__init__.py
+++ diffeqpy/__init__.py
@@ -1,6 +1,23 @@
+import os
+
 from julia import Julia
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
 
 def setup():
     jul = Julia()
-    jul.using("DiffEqPy")
+
+    # Load ./setup.jl
+    include = jul.eval('include')  # jul.include does not work sometimes
+    include(os.path.join(script_dir, 'setup.jl'))
+
+    # Make Julia functions and types exported from
+    # DifferentialEquations accessible:
+    jul.add_module_functions('DifferentialEquations')
+
+    # pysolve has to be treated manually:
+    # See: https://github.com/JuliaPy/pyjulia/issues/117#issuecomment-323498621
+    jul.pysolve = jul.eval('pysolve')
+
     return jul
