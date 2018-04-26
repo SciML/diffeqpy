81b0f07593aeb6e72560bf51b230a0144f0c177f
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-25 23:25:22 -0700

    Set timeout for loading DiffEqPy

--- diffeqpy/__init__.py
+++ diffeqpy/__init__.py
@@ -1,6 +1,19 @@
+import os
+
 from julia import Julia
 
-def setup():
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+
+def setup(**kwargs):
     jul = Julia()
-    jul.using("DiffEqPy")
+
+    include = jul.eval('include')  # jul.include does not work sometimes
+    include(os.path.join(script_dir, 'setup.jl'))
+    jl_setup = jul.eval('setup')
+    ok = jl_setup(**kwargs)
+    if not ok:
+        raise RuntimeError('Timeout reached while loading DiffEqPy.jl')
+
+    jul.add_module_functions('DiffEqPy')
     return jul
