ad289154df70a632c9f853f7123f1440c742122a
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-27 19:25:25 -0700

    Workaround barrage of UndefVarError messages

--- diffeqpy/de.py
+++ diffeqpy/de.py
@@ -1,4 +1,15 @@
 from . import setup
 _jul = setup()
-from julia.DifferentialEquations import *
+
+# Suppress many UndefVarError printed out.
+# See: https://github.com/JuliaPy/pyjulia/issues/159
+#      https://github.com/JuliaDiffEq/diffeqpy/pull/24
+try:
+    show = _jul.api.show
+    _jul.api.show = _jul._call('identity')
+    from julia.DifferentialEquations import *
+finally:
+    _jul.api.show = show
+del show
+
 solve = _jul.pysolve
