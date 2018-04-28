c2a6b3814b8e04279f41bccf184e70c1e358c1cb
Takafumi Arakaki <aka.tkf@gmail.com>
2018-04-27 20:16:30 -0700

    Comment on how monkey-patch works

--- diffeqpy/de.py
+++ diffeqpy/de.py
@@ -1,7 +1,12 @@
 from . import setup
 _jul = setup()
 
-# Suppress many UndefVarError printed out.
+# Suppress many UndefVarError printed out.  pyjulia calls
+# self.api.jl_call2(self.api.show, stderr, exoc) in
+# Julia.check_exception before raising JuliaError.  Although pyjulia
+# uses try-except to catch JuliaError, it still calls `self.api.show`
+# (which is `Base.show` in Julia).  Here we monkey-patch `Julia.api`
+# so that `api.show` points to `tuple` which does not print anything.
 # See: https://github.com/JuliaPy/pyjulia/issues/159
 #      https://github.com/JuliaDiffEq/diffeqpy/pull/24
 try:
