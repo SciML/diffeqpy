433e4ca4d4eb5c25f18b24e2f5f80b7c8a1bfb8d
Takafumi Arakaki <aka.tkf@gmail.com>
2018-12-20 21:01:15 -0800

    Show build log in CI

--- diffeqpy/install.jl
+++ diffeqpy/install.jl
@@ -1,6 +1,20 @@
 using Pkg
-Pkg.add("DifferentialEquations")
-Pkg.add("DiffEqBase")
-Pkg.add("PyCall")
+Pkg.add([
+    "DifferentialEquations",
+    "DiffEqBase",
+    "PyCall",
+])
 using DifferentialEquations
 using PyCall
+
+if lowercase(get(ENV, "CI", "false")) == "true"
+    @info "PyCall/deps/build.log:"
+    print(read(
+        joinpath(dirname(dirname(pathof(PyCall))), "deps", "build.log"),
+        String))
+    @info "Conda/deps/build.log:"
+    print(read(
+        joinpath(dirname(dirname(Base.find_package("Conda"))),
+                 "deps", "build.log"),
+        String))
+end
