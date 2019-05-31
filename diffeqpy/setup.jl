5bc9867e99a17d93b24998cbd02b794250b401ec
Christopher Rackauckas <accounts@chrisrackauckas.com>
2019-05-31 12:42:16 -0400

    Update setup.jl

--- diffeqpy/setup.jl
+++ diffeqpy/setup.jl
@@ -6,7 +6,7 @@ PyCall.PyObject(::typeof(DiffEqBase.solve)) =
 
 function DiffEqBase.numargs(f::PyCall.PyObject)
     inspect = PyCall.pyimport("inspect")
-    hasproperty(f,:py_func) ? _f = f.py_func : _f = f
+    PyCall.hasproperty(f,:py_func) ? _f = f.py_func : _f = f
     if PyCall.pyversion < v"3.0.0"
         return length(first(inspect.getargspec(_f)))
     else
