bcaf0a0cc1e79eb5390a7ac0cf174e03a34d14af
Christopher Rackauckas <accounts@chrisrackauckas.com>
2019-05-30 21:02:03 -0400

    Switch to property overloaded PyCall
    Fixes hasproperty

--- diffeqpy/setup.jl
+++ diffeqpy/setup.jl
@@ -6,10 +6,10 @@ PyCall.PyObject(::typeof(DiffEqBase.solve)) =
 
 function DiffEqBase.numargs(f::PyCall.PyObject)
     inspect = PyCall.pyimport("inspect")
-    haskey(f,:py_func) ? _f = f[:py_func] : _f = f
+    hasproperty(f,:py_func) ? _f = f.py_func : _f = f
     if PyCall.pyversion < v"3.0.0"
-        return length(first(inspect[:getargspec](_f)))
+        return length(first(inspect.getargspec(_f)))
     else
-        return length(first(inspect[:getfullargspec](_f)))
+        return length(first(inspect.getfullargspec(_f)))
     end
 end
