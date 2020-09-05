4985a98542d8ca69323bf190d340dc19e619dd31
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-04 20:54:03 -0700

    xfail: Revert "Handle bound method"
    This reverts commit 8e32ce964cb661b2f41d8770fca44510fe4a1054.

--- diffeqpy/setup.jl
+++ diffeqpy/setup.jl
@@ -7,13 +7,5 @@ PyCall.PyObject(::typeof(DiffEqBase.solve)) =
 function DiffEqBase.numargs(f::PyCall.PyObject)
     inspect = PyCall.pyimport("inspect")
     PyCall.hasproperty(f,:py_func) ? _f = f.py_func : _f = f
-    nargs = length(first(inspect.getfullargspec(_f)))
-    if inspect.ismethod(_f)
-        # `f` is a bound method (i.e., `self.f`) but `getfullargspec`
-        # includes `self` in the `args` list.  So, we are subtracting
-        # 1 manually here:
-        return nargs - 1
-    else
-        return nargs
-    end
+    return length(first(inspect.getfullargspec(_f)))
 end
