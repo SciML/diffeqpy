8e32ce964cb661b2f41d8770fca44510fe4a1054
Takafumi Arakaki <aka.tkf@gmail.com>
2020-09-04 19:24:40 -0700

    Handle bound method

--- diffeqpy/setup.jl
+++ diffeqpy/setup.jl
@@ -7,5 +7,13 @@ PyCall.PyObject(::typeof(DiffEqBase.solve)) =
 function DiffEqBase.numargs(f::PyCall.PyObject)
     inspect = PyCall.pyimport("inspect")
     PyCall.hasproperty(f,:py_func) ? _f = f.py_func : _f = f
-    return length(first(inspect.getfullargspec(_f)))
+    nargs = length(first(inspect.getfullargspec(_f)))
+    if inspect.ismethod(_f)
+        # `f` is a bound method (i.e., `self.f`) but `getfullargspec`
+        # includes `self` in the `args` list.  So, we are subtracting
+        # 1 manually here:
+        return nargs - 1
+    else
+        return nargs
+    end
 end
