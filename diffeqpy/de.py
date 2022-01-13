f8ac4ed78e90035a34c8615b4ef58a7e943db169
Takafumi Arakaki <aka.tkf@gmail.com>
2022-01-12 19:57:20 -0500

    Initialize PyJulia after `_ensure_installed()`

--- diffeqpy/de.py
+++ diffeqpy/de.py
@@ -1,12 +1,13 @@
 import os
 import sys
 
-from julia import Main
-
 from . import _ensure_installed
 
 # This is terrifying to many people. However, it seems SciML takes pragmatic approach.
 _ensure_installed()
+
+# PyJulia have to be loaded after `_ensure_installed()`
+from julia import Main
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 Main.include(os.path.join(script_dir, "setup.jl"))
