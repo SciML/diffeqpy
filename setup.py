7bd4d2e6152316991abf9f299ebb60273b7e0bd3
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-01-13 22:07:11 -0500

    Require latest julia_project to fix bug
    There is another bug fix in find_julia

--- setup.py
+++ setup.py
@@ -31,6 +31,6 @@ setup(name='diffeqpy',
       license='MIT',
       packages=['diffeqpy','diffeqpy.tests'],
       install_requires=['julia>=0.2',
-                        'julia_project>=0.0.23'],
+                        'julia_project>=0.0.24'],
       include_package_data=True,
       zip_safe=False)
