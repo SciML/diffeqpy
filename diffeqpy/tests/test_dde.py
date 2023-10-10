80af3ddbe2a58719cd4c954bbca0fd0fbc8e2730
Lilith Hafner <Lilith.Hafner@gmail.com>
2023-10-09 21:07:06 -0500

    fix a test failure BREAKING

--- diffeqpy/tests/test_dde.py
+++ diffeqpy/tests/test_dde.py
@@ -4,14 +4,14 @@ from .. import de
 
 
 def test():
-    f = Main.eval("""
+    f = de.seval("""
     function f(du, u, h, p, t)
       du[1] = 1.1/(1 + sqrt(10)*(h(p, t-20)[1])^(5/4)) - 10*u[1]/(1 + 40*u[2])
       du[2] = 100*u[1]/(1 + 40*u[2]) - 2.43*u[2]
     end""")
     u0 = [1.05767027/3, 1.030713491/3]
 
-    h = Main.eval("""
+    h = de.seval("""
     function h(p,t)
       [1.05767027/3, 1.030713491/3]
     end
