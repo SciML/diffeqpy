92bbdd6ec66272776cc4956a7bb911469fae4059
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-10-10 23:16:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='diffeqpy',
-      version='1.2.0',
+      version='2.0.0',
       description='Solving Differential Equations in Python',
       long_description=readme(),
       long_description_content_type="text/markdown",
