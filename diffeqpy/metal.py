3af7c4f663932235f8bf2552cbac39319b80dc0a
Lilith Orion Hafner <lilithhafner@gmail.com>
2023-12-06 08:25:25 -0600

    Make package installation automatic and deprecate manual installation

--- diffeqpy/metal.py
+++ diffeqpy/metal.py
@@ -1,6 +1,6 @@
 import sys
 from . import load_julia_packages
-metal, _ = load_julia_packages("DiffEqGPU, Metal")
+metal, _ = load_julia_packages("DiffEqGPU", "Metal")
 from juliacall import Main
 metal.MetalBackend = Main.seval("Metal.MetalBackend") # kinda hacky
 sys.modules[__name__] = metal # mutate myself
