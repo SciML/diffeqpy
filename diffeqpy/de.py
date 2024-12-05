657f2f6237754f335a7f22806b3c2979e76996c6
Christopher Rackauckas <accounts@chrisrackauckas.com>
2024-12-05 16:16:25 -0700

    2.5.3

--- diffeqpy/de.py
+++ diffeqpy/de.py
@@ -2,11 +2,11 @@ import sys
 from . import load_julia_packages
 de, _, _ = load_julia_packages("DifferentialEquations", "ModelingToolkit", "PythonCall")
 from juliacall import Main
-de.jit = Main.seval("global jit(x) = typeof(x).name.wrapper(ModelingToolkit.complete(ModelingToolkit.modelingtoolkitize(x); split = false), [], x.tspan)") # kinda hackey
-de.jit32 = Main.seval("""
-                      global function jit(x)
-                          prob = typeof(x).name.wrapper(ModelingToolkit.complete(ModelingToolkit.modelingtoolkitize(x); split = false), [], Float32.(x.tspan))
-                          ModelingToolkit.remake(prob; u0 = Float32.(prob.u0), p = Float32.(prob.p))
+de.seval("jit(x) = typeof(x).name.wrapper(Main.ModelingToolkit.complete(Main.ModelingToolkit.modelingtoolkitize(x); split = false), [], x.tspan)") # kinda hackey
+de.seval("""
+                      function jit(x)
+                          prob = typeof(x).name.wrapper(Main.ModelingToolkit.complete(Main.ModelingToolkit.modelingtoolkitize(x); split = false), [], Float32.(x.tspan))
+                          Main.ModelingToolkit.remake(prob; u0 = Float32.(prob.u0), p = Float32.(prob.p))
                       end
                       """) # kinda hackey
 sys.modules[__name__] = de # mutate myself
