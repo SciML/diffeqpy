b1ac4f5be00cdf5388c6b6519fca0868a57239b7
Christopher Rackauckas <accounts@chrisrackauckas.com>
2025-10-16 08:54:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(name='diffeqpy',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
